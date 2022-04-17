2d77764ec3b371112abcbb447c0ede27b1a617de
TyHafen <tyhafen@gmail.com>
2022-04-17 10:42:12 -0600

    profiles page

---
+++ Keepr.client/src/pages/ProfilesPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container-fluid">
+    <div class="row m-2 justify-content-start p-3">
+      <div class="col-md-1 p-2 align-items-end d-flex">
+        <img class="rounded" :src="profile.picture" alt="" />
+      </div>
+      <div class="col-md-9">
+        <h1>{{ profile.name }}</h1>
+        <h3>Vaults: {{ profileVaults.length }}</h3>
+        <h3>Keeps: {{ profileKeeps.length }}</h3>
+      </div>
+    </div>
+    <div class="row mt-5 m-3 justify-content-around">
+      <h1>Vaults <i class="mdi mdi-plus" @click=""></i></h1>
+      <div class="col-md-2 m-1" v-for="v in profileVaults" :key="v.id">
+        <h4>{{ v.name }}</h4>
+      </div>
+    </div>
+    <div class="mt-5 m-3">
+      <h1>Keeps <i class="mdi mdi-plus"></i></h1>
+      <div class="masonry">
+        <div class="col-md-12 p-3" v-for="k in profileKeeps" :key="k.id">
+          <Keep :keep="k" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, onMounted } from 'vue'
+import { AppState } from '../AppState'
+import { useRoute } from 'vue-router'
+import { logger } from '../utils/Logger'
+import Pop from '../utils/Pop'
+import { profilesService } from '../services/ProfilesService.js'
+import { accountService } from '../services/AccountService.js'
+export default {
+  setup() {
+    const route = useRoute()
+    onMounted(async () => {
+      try {
+        await profilesService.getProfile(route.params.id)
+        await profilesService.getProfilesVaults(route.params.id)
+        await profilesService.getProfilesKeeps(route.params.id)
+      } catch (error) {
+        logger.error(error)
+        Pop.toast(error.message, 'error')
+      }
+    })
+    return {
+      profile: computed(() => AppState.profile),
+      profileVaults: computed(() => AppState.profileVaults),
+      profileKeeps: computed(() => AppState.profileKeeps)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.masonry {
+  columns: 6 220px;
+  column-gap: 1rem;
+
+  div {
+    display: inline-block;
+    width: 100%;
+  }
+}
+</style>
